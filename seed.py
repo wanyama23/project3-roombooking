b13e6f2e33d81eb9bcd96c524ddfa7f3de864f64
edmond <edmond.wanyama@student.moringaschool.com>
2023-09-07 00:48:06 +0300

    adding customes

--- seed.py
+++ seed.py
@@ -6,4 +6,21 @@ import random
 
 from models import Booking, Customer, Room,room_customer
 if __name__ == "__main__":
-    
+    fake = Faker()
+    engine = create_engine('sqlite:///roombooking.db')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.query(Booking).delete()
+    session.query(Customer).delete()
+    session.query(Room).delete()
+    session.query(room_customer).delete()
+
+    customers = []
+    for _ in range (20):
+        customer= Customer(
+            book_name=fake.name(),
+            phone_number=fake.phone_number(),
+            email=fake.email()
+        )
+        session.add(customer)
+        customers.append(customer)
