a9f2f8af47a60c3d31fd37b63163161c7a625c61
edmond <edmond.wanyama@student.moringaschool.com>
2023-09-06 17:21:20 +0300

    adding sesseions

--- models.py
+++ models.py
@@ -1,6 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import (Column,Integer,Date,String,ForeignKey,Table)
-engine = create_engine('sqlite:///roombooking.db')
+from sqlalchemy.orm import relationship,sessionmaker,backref
 
 Base = declarative_base()
+
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///roombooking.db')
+    Session = sessionmaker(bind=engine)
+    session = Session()
